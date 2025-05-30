6f0e0bf9908302cf55ca2bd17003fb9bf6c75cef
SairamNagarajan1 <databytesairam@gmail.com>
2025-05-30 06:47:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ import joblib
 app = Flask(__name__)
 model = joblib.load("iris_model.pkl")
 
+@app.route('/')
+def home():
+    return "Welcome to the Iris Classifier API!"
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     features = data['features']
     prediction = model.predict([features])
     return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
